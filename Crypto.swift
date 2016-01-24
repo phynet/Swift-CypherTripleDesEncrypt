8c7c22cae232e9b5b0d9fd733ea2830c696d3711
phynet <sofia.swidarowicz@gmail.com>
2016-01-24 16:59:19 +0100

    Added Files

---
+++ Crypto.swift
@@ -0,0 +1,70 @@
+//
+//  Crypto.swift
+//  SimyoAdaptation
+//
+//  Created by Sofia Swidarowicz on 25/10/15.
+//  Copyright © 2015 Sofia Swidarowicz. All rights reserved.
+//
+
+import UIKit
+import CryptoSwift
+
+enum CryptoAlgorithm {
+    case MD5, SHA1, SHA224, SHA256, SHA384, SHA512
+    
+    var HMACAlgorithm: CCHmacAlgorithm {
+        var result: Int = 0
+        switch self {
+        case .MD5:      result = kCCHmacAlgMD5
+        case .SHA1:     result = kCCHmacAlgSHA1
+        case .SHA224:   result = kCCHmacAlgSHA224
+        case .SHA256:   result = kCCHmacAlgSHA256
+        case .SHA384:   result = kCCHmacAlgSHA384
+        case .SHA512:   result = kCCHmacAlgSHA512
+        }
+        return CCHmacAlgorithm(result)
+    }
+    
+    var digestLength: Int {
+        var result: Int32 = 0
+        switch self {
+        case .MD5:      result = CC_MD5_DIGEST_LENGTH
+        case .SHA1:     result = CC_SHA1_DIGEST_LENGTH
+        case .SHA224:   result = CC_SHA224_DIGEST_LENGTH
+        case .SHA256:   result = CC_SHA256_DIGEST_LENGTH
+        case .SHA384:   result = CC_SHA384_DIGEST_LENGTH
+        case .SHA512:   result = CC_SHA512_DIGEST_LENGTH
+        }
+        return Int(result)
+    }
+}
+
+extension String {
+    
+    func hmac(algorithm: CryptoAlgorithm, key: String) -> String {
+        let str = self.cStringUsingEncoding(NSUTF8StringEncoding)
+        let strLen = Int(self.lengthOfBytesUsingEncoding(NSUTF8StringEncoding))
+        let digestLen = algorithm.digestLength
+        let result = UnsafeMutablePointer<CUnsignedChar>.alloc(digestLen)
+        let keyStr = key.cStringUsingEncoding(NSUTF8StringEncoding)
+        let keyLen = Int(key.lengthOfBytesUsingEncoding(NSUTF8StringEncoding))
+        
+        CCHmac(algorithm.HMACAlgorithm, keyStr!, keyLen, str!, strLen, result)
+        
+        let digest = stringFromResult(result, length: digestLen)
+        
+        result.dealloc(digestLen)
+        
+        return digest
+    }
+    
+    private func stringFromResult(result: UnsafeMutablePointer<CUnsignedChar>, length: Int) -> String {
+        let hash = NSMutableString()
+        for i in 0..<length {
+            hash.appendFormat("%02x", result[i])
+        }
+        return String(hash)
+    }
+    
+ 
+}
---
+++ CypherSwift.swift
@@ -0,0 +1,68 @@
+//
+//  CypherSwift.swift
+//  SimyoAdaptation
+//
+//  Created by Sofia Swidarowicz on 01/11/15.
+//  Copyright © 2015 Sofia Swidarowicz. All rights reserved.
+//
+
+import UIKit
+import CryptoSwift
+
+
+class CypherSwift: NSObject {
+    
+    
+    func tripleDesEncrypt(pass: String) -> String{
+        //help from this thread
+        //http://stackoverflow.com/questions/25754147/issue-using-cccrypt-commoncrypt-in-swift
+        
+        let keyString        = "25d1d4cb0a08403e2acbcbe0"
+        let keyData: NSData! = (keyString as NSString).dataUsingEncoding(NSUTF8StringEncoding) as NSData!
+        
+        let message       = pass
+        let data: NSData! = (message as NSString).dataUsingEncoding(NSUTF8StringEncoding) as NSData!
+        
+        let cryptData    = NSMutableData(length: Int(data.length) + kCCBlockSize3DES)!
+
+        let keyLength              = size_t(kCCKeySize3DES)
+        let operation: CCOperation = UInt32(kCCEncrypt)
+        let algoritm:  CCAlgorithm = UInt32(kCCAlgorithm3DES)
+        let options:   CCOptions   = UInt32(kCCOptionECBMode + kCCOptionPKCS7Padding)
+        
+        var numBytesEncrypted :size_t = 0
+        
+        let cryptStatus = CCCrypt(operation,
+            algoritm,
+            options,
+            keyData.bytes, keyLength,
+            nil,
+            data.bytes, data.length,
+            cryptData.mutableBytes, cryptData.length,
+            &numBytesEncrypted)
+        
+        if UInt32(cryptStatus) == UInt32(kCCSuccess) {
+            cryptData.length = Int(numBytesEncrypted)
+            
+            // Not all data is a UTF-8 string so Base64 is used
+            var base64cryptString = cryptData.base64EncodedStringWithOptions(.Encoding64CharacterLineLength)
+            
+            print("base64cryptString = \(base64cryptString)")
+            base64cryptString = base64cryptString + "\n"
+            return encodeString(base64cryptString)
+        
+        } else {
+            print("Error: \(cryptStatus)")
+        }
+        return ""
+    }
+    
+    func encodeString(str: String) -> String{
+        let customAllowedSet =  NSCharacterSet(charactersInString:"==\n").invertedSet
+        let escapedString = str.stringByAddingPercentEncodingWithAllowedCharacters(customAllowedSet)
+        print("escapedString: \(escapedString)")
+        return escapedString!
+    
+    }
+
+}
